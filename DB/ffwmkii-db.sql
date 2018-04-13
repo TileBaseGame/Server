b13008935772af6243972b4cc107f85f54958522
Alonzo <alonzocontreras14@gmail.com>
2018-04-13 14:48:07 -0700

    changed syntax from mysql to postgres

--- DB/ffwmkii-db.sql
+++ DB/ffwmkii-db.sql
@@ -1,28 +1,12 @@
-CREATE TABLE abilities (ability_id INT NOT NULL AUTO_INCREMENT,
-			description VARCHAR(255) NOT NULL,
-			unit_usable BOOLEAN,
-			ability_name VARCHAR(55) NOT NULL,
-			PRIMARY KEY(ability_id));
+CREATE TABLE abilities (ability_id SERIAL PRIMARY KEY NOT NULL, description VARCHAR(255) NOT NULL, unit_usable BOOLEAN,	ability_name VARCHAR(55) NOT NULL);
 
-CREATE TABLE users (user_id INT NOT NULL AUTO_INCREMENT,
-			email VARCHAR(55),
-			username VARCHAR(55),
-			password TEXT,
-			PRIMARY KEY(user_id));
+CREATE TABLE users (user_id SERIAL PRIMARY KEY NOT NULL, email VARCHAR(55), username VARCHAR(55), password TEXT);
 
-CREATE TABLE units (unit_id INT NOT NULL AUTO_INCREMENT,
-			unit_type VARCHAR(45),
-			hp INT NOT NULL,
-			PRIMARY KEY(unit_id));
+CREATE TABLE units (unit_id  SERIAL PRIMARY KEY NOT NULL, unit_type VARCHAR(45), hp INT NOT NULL);
 
-CREATE TABLE user_units (user_unit_id INT NOT NULL,
-			unit_x INT,
-			unit_y INT,
-			PRIMARY KEY (user_unit_id),
-			FOREIGN KEY (unit_id) REFERENCES (units),
-			FOREIGN KEY (ability_id) REFERENCES (abilities),
-			FOREIGN KEY (user_id) REFERENCES (users));
+CREATE TABLE user_units (user_unit_id INT PRIMARY KEY NOT NULL, unit_x INT, unit_y INT, unit_id INT REFERENCES units (unit_id), ability_id INT REFERENCES abilities (ability_id), user_id INT REFERENCES users(user_id));
 
-CREATE TABLE game (game_id INT NOT NULL,
-			PRIMARY KEY (game_id),
-			FOREIGN KEY (user_unit_id) REFERENCES (user_units));
+CREATE TABLE game (game_id INT NOT NULL PRIMARY KEY, user_unit_id INT REFERENCES user_units(user_unit_id));
+
+/*INSERT INTO units (unit_type, hp) values ('support', 20);*/
+
