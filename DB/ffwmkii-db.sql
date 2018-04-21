44dd509c47da28408cca73997881d284b7a6852c
alonzo <alonzocontreras14@gmail.com>
2018-04-20 19:24:38 -0700

    added INSERT commands in .sql

--- DB/ffwmkii-db.sql
+++ DB/ffwmkii-db.sql
@@ -8,5 +8,16 @@ CREATE TABLE user_units (user_unit_id INT PRIMARY KEY NOT NULL, unit_x INT, unit
 
 CREATE TABLE game (game_id INT NOT NULL PRIMARY KEY, user_unit_id INT REFERENCES user_units(user_unit_id));
 
-/*INSERT INTO units (unit_type, hp) values ('support', 20);*/
+-- INSERT INTO units (unit_type, hp) values ('melee', 20);
 
+-- INSERT INTO units (unit_type, hp) values ('range', 17);
+
+-- INSERT INTO units (unit_type, hp) values ('support', 14);
+
+-- INSERT INTO abilities (description, unit_usable, ability_name) values ('Poisons an unit doing 50% attack damage over 4 turns', False, 'Poison');
+
+-- INSERT INTO abilities (description, unit_usable, ability_name) values ('Shoots 6 volleys of missiles, each volley does 25% damage', False, 'Arcane Missiles');
+
+-- INSERT INTO abilities (description, unit_usable, ability_name) values ('do 200% damage', False, 'Glacial Shard');
+
+-- INSERT INTO abilities (description, unit_usable, ability_name) values ('Does 150% attack healing', False, 'Restoritive Seronade');
